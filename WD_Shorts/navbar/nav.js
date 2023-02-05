db296daa26a89591bc595ecac9c080cd672b5208
mdfaizan973 <faizan.md9735@gmail.com>
2023-02-06 02:18:18 +0530

    added

---
+++ WD_Shorts/navbar/nav.js
@@ -0,0 +1,14 @@
+// let h1 = document.querySelector('h1')
+
+// function mouseOver() {
+//    h1.style.color = "white";
+//    h1.innerHTML='Faizan'
+//   }
+  
+//   function mouseOut() {
+//     h1.style.color = "black";
+//    h1.innerHTML='Navbar'
+
+//   }
+
+// id="demo" onmouseover="mouseOver()" onmouseout="mouseOut()"
